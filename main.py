e84718a1ada22ce64db8c903f66b48a221ed982b
Hugo PANEL <hugopanel@gmail.com>
2023-03-18 16:34:57 +0100

    Ajout alpha et omega + reformating de certaines varibles pour être plus clair.

--- main.py
+++ main.py
@@ -2,37 +2,38 @@ import pandas as pd
 import numpy as np
 from graphes import *
 
-if __name__ == "__main__":
+
+def read_file(file_path: str):
     # Ouverture du fichier
-    file = "./files/table 1.txt"
-    f = open(file, 'r')
+    # file = "./files/table 2.txt"
+    f = open(file_path, 'r')
 
     graph = Graphe()  # Initialisation du graphe
-    l = f.readline()  # Lecture du fichier
+    line = f.readline()  # Lecture du fichier
 
-    while l:
-        l = l.strip()  # Supprimer les espaces et retours à la ligne au début et à la fin
-        c = l.split(' ')  # Découper la ligne en tableau à chaque espace
+    while line:
+        line = line.strip()  # Supprimer les espaces et retours à la ligne au début et à la fin
+        column = line.split(' ')  # Découper la ligne en tableau à chaque espace
 
         # On vérifie si la tâche a déjà été ajoutée au graphe
         task_exists = False
         for tache in graph.taches:
-            if tache.nom == c[0]:
+            if tache.nom == column[0]:
                 task_exists = True
-                t = tache  # On récupère la tâche pour la modifier
+                task = tache  # On récupère la tâche pour la modifier
                 break  # On quitte la boucle
         if not task_exists:
             # La tâche n'a pas été ajoutée, donc on la crée :
-            t = Tache()
-            t.nom = c[0]
+            task = Tache()
+            task.nom = column[0]
             # graph.taches = np.append(graph.taches, [t])  # On ajoute la tâche au graphe
 
         # On modifie la tâche en fonction de ce qu'on lit dans le fichier .txt :
-        t.duree = c[1]
+        task.duree = column[1]
 
         # S'il existe des contraintes (prédécesseurs), on les ajoute :
-        if len(c) > 2:
-            for pre in c[2:]:  # Pour chaque contrainte...
+        if len(column) > 2:
+            for pre in column[2:]:  # Pour chaque contrainte...
                 # On vérifie si elle a déjà été ajoutée au graphe
                 pred_exists = False
                 for tache in graph.taches:
@@ -47,8 +48,66 @@ if __name__ == "__main__":
                     graph.taches = np.append(graph.taches, [predecesseur])  # On l'ajoute au graphe
 
                 # On ajoute la contrainte à la liste des contraintes :
-                t.predecesseurs = np.append(t.predecesseurs, [predecesseur])
+                task.predecesseurs = np.append(task.predecesseurs, [predecesseur])
 
         if not task_exists:
-            graph.taches = np.append(graph.taches, [t])
-        l = f.readline()
+            graph.taches = np.append(graph.taches, [task])
+
+        line = f.readline()
+    return graph
+
+# TODO: Ajouter les successeurs ???
+
+
+def create_alpha(graphe: Graphe):
+    """
+    Crée la tâche alpha et l'ajoute au tableau de tâches du graphe.
+
+    :param graphe: Le graphe
+    :return: Le graphe avec la tâche alpha (0).
+    """
+    # Regarder les tâches qui n'ont aucune contrainte
+    alpha = Tache()
+    alpha.nom = 0
+    alpha.duree = 0
+
+    for tache in graphe.taches:
+        if tache.predecesseurs is None:
+            tache.predecesseurs = np.array([alpha])
+
+    graphe.taches = np.append(graphe.taches, [alpha])
+    return graphe
+
+
+def create_omega(graphe: Graphe):
+    """
+    Crée la tâche omega et l'ajoute au tableau de tâches du graphe.
+
+    :param graphe: Le graphe
+    :return: Le graphe avec la tâche omega (N+1)
+    """
+    # Regarder les tâches qui n'apparaissent pas dans les contraintes
+    taches_sans_successeurs = []
+    for tache in graphe.taches:
+        taches_sans_successeurs.append(tache)
+
+    for tache in graphe.taches:
+        if tache.predecesseurs is not None:
+            for pred in tache.predecesseurs:
+                if pred in taches_sans_successeurs:
+                    taches_sans_successeurs.remove(pred)
+
+    omega = Tache()
+    omega.nom = len(graphe.taches) + 1
+    omega.duree = 0
+
+    omega.predecesseurs = taches_sans_successeurs
+
+    graphe.taches = np.append(graphe.taches, [omega])
+    return graphe
+
+
+if __name__ == "__main__":
+    graphe = read_file("./files/table 1.txt")
+    graphe = create_alpha(graphe)
+    graphe = create_omega(graphe)
